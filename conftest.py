893f759e2c8c885d0bba06e6f0e4756009083cee
Vadzim Belashtsentau <vadzimbelashtsentau@gmail.com>
2023-11-09 19:56:03 +0300

    conftest update for 8_14

--- conftest.py
+++ conftest.py
@@ -1,4 +1,6 @@
 import pytest
+import os
+from dotenv import load_dotenv
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -6,8 +8,15 @@ from selene import browser
 
 from utils import attach
 
+def pytest_addoption(parser):
+    parser.addoption('--remote_url', default='')
+
 @pytest.fixture(scope='function', autouse=True)
-def browser_setup():
+def load_env():
+    load_dotenv()
+
+@pytest.fixture(scope='function', autouse=True)
+def browser_setup(request):
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -18,8 +27,11 @@ def browser_setup():
         }
     }
     options.capabilities.update(selenoid_capabilities)
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+    remote_url = request.config.getoption('--remote_url')
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@{remote_url}",
         options=options
     )
 
